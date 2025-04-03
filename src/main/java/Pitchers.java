2ecfe5eb621c5d9f92deb596bfa39091ed53d915
ShangRu Yu <yu@youshangrudeMacBook-Air.local>
2025-04-03 10:29:00 -0700

    upload player data

--- src/main/java/Pitchers.java
+++
@@ -1,9 +0,0 @@
-import java.util.Map;
-
-public interface Pitchers {
-    String getName();
-    int getRotation();
-    double getStrikesRate();
-    double getBallsRate();
-    Map<String, Double> getPitchUsage();
-}
