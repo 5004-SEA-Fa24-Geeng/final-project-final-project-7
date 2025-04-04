ef6b62c9e03fc9cb3bf905ff98c18383ffdeae76
ShangRu Yu <yu@youshangrudeMacBook-Air.local>
2025-04-04 15:09:37 -0700

    feat: modify fields for Batter, Pitcher

--- src/main/java/model/player/Batter.java
+++ src/main/java/model/player/Batter.java
@@ -1,75 +1,201 @@
 package model.player;
 
 public class Batter extends Player{
-    private String pitchType;
-    private int plateAppearances;
-    private int hits;
-    private int singles;
-    private int doubles;
-    private int triples;
-    private int homeRuns;
+    private int fastballPA;
+    private int fastballH;
+    private int fastball1B;
+    private int fastball2B;
+    private int fastball3B;
+    private int fastballHR;
+    private int breakingPA;
+    private int breakingH;
+    private int breaking1B;
+    private int breaking2B;
+    private int breaking3B;
+    private int breakingHR;
+    private int offspeedPA;
+    private int offspeedH;
+    private int offspeed1B;
+    private int offspeed2B;
+    private int offspeed3B;
+    private int offspeedHR;
+    private int totalPA;
+    private int totalH;
+    private int total1B;
+    private int total2B;
+    private int total3B;
+    private int totalHR;
     private double zoneSwing;
     private double zoneContact;
     private double chase;
     private double chaseContact;
+    private double AVG;
+    private double OBP;
+    private double OPS;
 
-    public Batter(String name, String pitchType, int plateAppearances, int hits, int singles, int doubles,
-                  int triples, int homeRuns, double zoneSwing, double zoneContact, double chase, double chaseContact) {
+    public Batter(String name, int fastballPA, int fastballH, int fastball1B, int fastball2B, int fastball3B,
+                  int fastballHR, int breakingPA, int breakingH, int breaking1B, int breaking2B, int breaking3B,
+                  int breakingHR, int offspeedPA, int offspeedH, int offspeed1B, int offspeed2B, int offspeed3B,
+                  int offspeedHR, int totalPA, int totalH, int total1B, int total2B, int total3B, int totalHR,
+                  double zoneSwing, double zoneContact, double chase, double chaseContact, double AVG,
+                  double OBP, double OPS) {
         super(name);
 
-        this.pitchType = pitchType;
-        this.plateAppearances = plateAppearances;
-        this.hits = hits;
-        this.singles = singles;
-        this.doubles = doubles;
-        this.triples = triples;
-        this.homeRuns = homeRuns;
+        this.fastballPA = fastballPA;
+        this.fastballH = fastballH;
+        this.fastball1B = fastball1B;
+        this.fastball2B = fastball2B;
+        this.fastball3B = fastball3B;
+        this.fastballHR = fastballHR;
+        this.breakingH = breakingH;
+        this.breakingPA = breakingPA;
+        this.breaking1B = breaking1B;
+        this.breaking2B = breaking2B;
+        this.breaking3B = breaking3B;
+        this.breakingHR = breakingHR;
+        this.offspeedPA = offspeedPA;
+        this.offspeedH = offspeedH;
+        this.offspeed1B = offspeed1B;
+        this.offspeed2B = offspeed2B;
+        this.offspeed3B = offspeed3B;
+        this.offspeedHR = offspeedHR;
+        this.totalPA = totalPA;
+        this.totalH = totalH;
+        this.total1B = total1B;
+        this.total2B = total2B;
+        this.total3B = total3B;
+        this.totalHR = totalHR;
         this.zoneSwing = zoneSwing;
         this.zoneContact = zoneContact;
         this.chase = chase;
         this.chaseContact = chaseContact;
+        this.AVG = AVG;
+        this.OBP = OBP;
+        this.OPS = OPS;
     }
 
-    public String getPitchType() {
-        return pitchType;
+    public int getFastballPA() {
+        return this.fastballPA;
     }
 
-    public int getPlateAppearances() {
-        return plateAppearances;
+    public int getFastballH() {
+        return this.fastballH;
     }
 
-    public int getHits() {
-        return hits;
+    public int getFastball1B() {
+        return this.fastball1B;
     }
 
-    public int getSingles() {
-        return singles;
+    public int getFastball2B() {
+        return this.fastball2B;
     }
 
-    public int getDoubles() {
-        return doubles;
+    public int getFastball3B() {
+        return this.fastball3B;
     }
 
-    public int getTriples() {
-        return triples;
+    public int getFastballHR() {
+        return this.fastballHR;
     }
 
-    public int getHomeRuns() {
-        return homeRuns;
+    public int getBreakingPA() {
+        return this.breakingPA;
+    }
+
+    public int getBreakingH() {
+        return this.breakingH;
+    }
+
+    public int getBreaking1B() {
+        return this.breaking1B;
+    }
+
+    public int getBreaking2B() {
+        return this.breaking2B;
+    }
+
+    public int getBreaking3B() {
+        return this.breaking3B;
+    }
+
+    public int getBreakingHR() {
+        return this.breakingHR;
+    }
+
+    public int getOffspeedPA() {
+        return this.offspeedPA;
+    }
+
+    public int getOffspeedH() {
+        return this.offspeedH;
+    }
+
+    public int getOffspeed1B() {
+        return this.offspeed1B;
+    }
+
+    public int getOffspeed2B() {
+        return this.offspeed2B;
+    }
+
+    public int getOffspeed3B() {
+        return this.offspeed3B;
+    }
+
+    public int getOffspeedHR() {
+        return this.offspeedHR;
+    }
+
+    public int getTotalPA() {
+        return this.totalPA;
+    }
+
+    public int getTotalH() {
+        return this.totalH;
+    }
+
+    public int getTotal1B() {
+        return this.total1B;
+    }
+
+    public int getTotal2B() {
+        return this.total2B;
+    }
+
+    public int getTotal3B() {
+        return this.total3B;
+    }
+
+    public int getTotalHR() {
+        return this.totalHR;
     }
 
     public double getZoneSwing() {
-        return zoneSwing;
+        return this.zoneSwing;
     }
+
     public double getZoneContact() {
-        return zoneContact;
+        return this.zoneContact;
     }
+
     public double getChase() {
-        return chase;
+        return this.chase;
     }
 
     public double getChaseContact() {
-        return chaseContact;
+        return this.chaseContact;
+    }
+
+    public double getAVG() {
+        return this.AVG;
+    }
+
+    public double getOBP() {
+        return this.OBP;
+    }
+
+    public double getOPS() {
+        return this.OPS;
     }
 
     @Override
--- src/main/java/model/player/Pitcher.java
+++ src/main/java/model/player/Pitcher.java
@@ -2,52 +2,120 @@ package model.player;
 
 public class Pitcher extends Player{
     private String rotation;
-    private String pitchName;
-    private double pitchUsage;
     private int strikes;
     private int pitches;
     private double strikesRate;
     private double ballsRate;
+    private double fourSeam;
+    private double twoSeam;
+    private double cutter;
+    private double sinker;
+    private double slider;
+    private double curve;
+    private double knuckle;
+    private double sweeper;
+    private double slurve;
+    private double splitFinger;
+    private double changeup;
+    private double fork;
+    private double screw;
 
-    public Pitcher(String name, String rotation, String pitchName, double pitchUsage,
-                   int strikes, int pitches, double strikesRate, double ballsRate) {
+    public Pitcher(String name, String rotation, int strikes, int pitches, double strikesRate, double ballsRate,
+                   double fourSeam, double twoSeam, double cutter, double sinker, double slider, double curve,
+                   double knuckle, double sweeper, double slurve, double splitFinger, double changeup, double fork,
+                   double screw) {
         super(name);
 
         this.rotation = rotation;
-        this.pitchName = pitchName;
-        this.pitchUsage = pitchUsage;
         this.strikes = strikes;
         this.pitches = pitches;
         this.strikesRate = strikesRate;
         this.ballsRate = ballsRate;
-
+        this.fourSeam = fourSeam;
+        this.twoSeam = twoSeam;
+        this.cutter = cutter;
+        this.sinker = sinker;
+        this.slider = slider;
+        this.curve = curve;
+        this.knuckle = knuckle;
+        this.sweeper =sweeper;
+        this.slurve = slurve;
+        this.splitFinger = splitFinger;
+        this.changeup = changeup;
+        this.fork = fork;
     }
 
     public String getRotation() {
-        return rotation;
+        return this.rotation;
     }
 
-    public String getPitchName() {
-        return pitchName;
-    }
-
-    public double getPitchUsage() {
-        return pitchUsage;
-    }
-
-    public int getStrikes() {return strikes;
+    public int getStrikes() {
+        return this.strikes;
     }
 
     public int getPitches() {
-        return pitches;
+        return this.pitches;
     }
 
     public double getStrikesRate() {
-        return strikesRate;
+        return this.strikesRate;
     }
 
     public double getBallsRate() {
-        return ballsRate;
+        return this.ballsRate;
+    }
+
+    public double getFourSeam() {
+        return this.fourSeam;
+    }
+
+    public double getTwoSeam() {
+        return this.twoSeam;
+    }
+
+
+    public double getCutter() {
+        return this.cutter;
+    }
+
+    public double getSinker() {
+        return this.sinker;
+    }
+
+    public double getSlider() {
+        return this.slider;
+    }
+
+    public double getCurve() {
+        return this.curve;
+    }
+
+    public double getKnuckle() {
+        return this.knuckle;
+    }
+
+    public double getSweeper() {
+        return this.sweeper;
+    }
+
+    public double getSlurve() {
+        return this.slurve;
+    }
+
+    public double getSplitFinger() {
+        return this.splitFinger;
+    }
+
+    public double getChangeup() {
+        return this.changeup;
+    }
+
+    public double getFork() {
+        return this.fork;
+    }
+
+    public double getScrew() {
+        return this.screw;
     }
 
     @Override
