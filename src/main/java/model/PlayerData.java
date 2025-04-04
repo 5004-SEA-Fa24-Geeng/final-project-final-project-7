a616535cd08c2a39e54cb95bb76ad08cdb7abb91
Huang Shou Liu <liu.huangs@northeastern.edu>
2025-04-03 19:31:04 -0700

    feat: add transformed pitcher data and new batter data; create Operations, PlayerData, Filter and PlayerFilter

---
+++ src/main/java/model/PlayerData.java
@@ -0,0 +1,91 @@
+package model;
+
+public enum PlayerData {
+
+    /**
+     * Enums matching CODE(cvsname) pattern.
+     */
+    NAME("Name"),
+    //Batters
+    F_PA("FastallPA"),
+    F_H("FastballH"), F_1B("Fastball1B"),
+    F_2B("Fastball2B"), F_3B("Fastball3B"),
+    F_HR("FastballHR"), B_PA("BreakingPA"),
+    B_H("BreakingH"), B_1B("Breaking1B"),
+    B_2B("Breaking2B"), B_3B("Breaking3B"),
+    B_HR("BreakingHR"), O_PA("OffspeedPA"),
+    O_H("OffspeedH"), O_1B("Offspeed1B"),
+    O_2B("Offspeed2B"), O_3B("Offspeed3B"),
+    O_HR("OffspeedHR"), T_PA("TotalPA"),
+    T_H("TotalH"), T_1B("Total1B"),
+    T_2B("Total2B"), T_3B("Total3B"),
+    T_HR("TotalHR"), Z_SWING("ZoneSwing"),
+    Z_CONTACT("ZoneContact"), C_SWING("ChaseSwing"),
+    C_CONTACT("ChaseContact"), AVG("AVG"),
+    OBP("OBP"), OPS("OPS"),
+    //Pitchers
+    ROTATION("Rotation"), STRIKES("Strikes"),
+    PITCHES("Pitches"), S_RATE("Strikes Rate"),
+    B_RATE("Balls Rate"), FOURSEAM("4-Seam Fastball"),
+    TWOSEAM("2-Seam Fastball"), CUTTER("Cutter"),
+    SINKER("Sinker"), SLIDER("Slider"),
+    CURVE("Curveball"), KNUCKLE("Knuckle"),
+    SWEEPER("Sweeper"), SLURVE("Slurve"),
+    SFINGER("Split-Finger"), CHANGEUP("Changeup"),
+    FORK("Fork"), SCREW("Screw");
+
+    /** stores the original csv name in the enum. */
+    private final String columnName;
+
+    /**
+     * Constructor for the enum.
+     * 
+     * @param columnName the name of the column in the CSV file.
+     */
+    PlayerData(String columnName) {
+        this.columnName = columnName;
+    }
+
+    /**
+     * Getter for the column name.
+     * 
+     * @return the name of the column in the CSV file.
+     */
+    public String getColumnName() {
+        return columnName;
+    }
+
+    /**
+     * Get the enum from the column name.
+     * 
+     * @param columnName the name of the column in the CSV file.
+     * @return the enum that matches the column name.
+     */
+    public static PlayerData fromColumnName(String columnName) {
+        for (PlayerData col : PlayerData.values()) {
+            if (col.getColumnName().equals(columnName)) {
+                return col;
+            }
+        }
+        throw new IllegalArgumentException("No column with name " + columnName);
+    }
+
+    /**
+     * Get the enum from the enum name.
+     * 
+     * Can use the enum name or the column name. Useful for filters and sorts
+     * as they can use both.
+     * 
+     * @param name the name of the enum.
+     * @return the enum that matches the name.
+     */
+    public static PlayerData fromString(String name) {
+        for (PlayerData col : PlayerData.values()) {
+            if (col.name().equalsIgnoreCase(name) || col.getColumnName().equalsIgnoreCase(name)) {
+                return col;
+            }
+        }
+        throw new IllegalArgumentException("No column with name " + name);
+    }
+
+}
--- src/main/java/model/filter/Filter.java
+++ src/main/java/model/filter/Filter.java
@@ -1,10 +1,55 @@
 package model.filter;
 
-import java.util.List;
-import java.util.stream.Stream;
-
+import model.Operations;
+import model.PlayerData;
 import model.player.Player;
 
 public interface Filter {
-    Stream<Player> filter(List<Player> players, String attribute, Object value);
+
+    /**
+     * filter batters using batter data, operations, and the value user send in.
+     * @param batter batter
+     * @param column player data name, ex: name, FastballPA etc
+     * @param op operations, ex: equals, greater then etc
+     * @param value the value used to compare with the value of a batter
+     * @return boolean, true if the batter fits the filter, else false
+     */
+    boolean batterFilter(Player batter, PlayerData column, Operations op, String value);
+
+    /**
+     * filter board games using game data, operations, and the value user send in.
+     * @param pitcher pitcher
+     * @param column player data name, ex: name, Strikes etc
+     * @param op operations, ex: equals, greater then etc
+     * @param value the value used to compare with the value of a pitcher
+     * @return boolean, true if the pitcher fits the filter, else false
+     */
+    boolean pitcherFilter(Player pitcher, PlayerData column, Operations op, String value);
+
+    /**
+     * Compares strings.
+     * @param playerData the name of the player
+     * @param op operations, ex: equals, greater then etc
+     * @param value the value used to compare with the value of a player
+     * @return boolean, true if the player fits the filter, else false
+     */
+    boolean filterString(String playerData, Operations op, String value);
+
+    /**
+     * Compares integers.
+     * @param playerData fields such as PA, Hits etc 
+     * @param op operations, ex: equals, greater then etc
+     * @param value the value used to compare with the value of a board game
+     * @return boolean, true if the game fits the filter, else false
+     */
+    boolean filterNum(int playerData, Operations op, String value);  
+
+    /**
+     * Compares double.
+     * @param playerData fields such as pitch usage of a pitch type
+     * @param op operations, ex: equals, greater then etc
+     * @param value the value used to compare with the value of a board game
+     * @return boolean, true if the game fits the filter, else false
+     */
+    boolean filterDouble(double playerData, Operations op, String value);        
 }
