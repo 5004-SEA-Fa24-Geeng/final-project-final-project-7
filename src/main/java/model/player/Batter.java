0606ee0a0951cccb1a91bee92298b5f6c97726c6
ShangRu Yu <yu@youshangrudeMacBook-Air.local>
2025-04-08 11:38:35 -0700

    feat: modify getters of Pitcher, Batter

--- src/main/java/model/player/Batter.java
+++ src/main/java/model/player/Batter.java
@@ -37,6 +37,41 @@ public class Batter extends Player{
     private double OBP;
     private double OPS;
 
+    /**
+     *
+     * @param name
+     * @param fastballPA
+     * @param fastballH
+     * @param fastball1B
+     * @param fastball2B
+     * @param fastball3B
+     * @param fastballHR
+     * @param breakingPA
+     * @param breakingH
+     * @param breaking1B
+     * @param breaking2B
+     * @param breaking3B
+     * @param breakingHR
+     * @param offspeedPA
+     * @param offspeedH
+     * @param offspeed1B
+     * @param offspeed2B
+     * @param offspeed3B
+     * @param offspeedHR
+     * @param totalPA
+     * @param totalH
+     * @param total1B
+     * @param total2B
+     * @param total3B
+     * @param totalHR
+     * @param zoneSwing
+     * @param zoneContact
+     * @param chaseSwing
+     * @param chaseContact
+     * @param AVG
+     * @param OBP
+     * @param OPS
+     */
     public Batter(String name, int fastballPA, int fastballH, int fastball1B, int fastball2B, int fastball3B,
                   int fastballHR, int breakingPA, int breakingH, int breaking1B, int breaking2B, int breaking3B,
                   int breakingHR, int offspeedPA, int offspeedH, int offspeed1B, int offspeed2B, int offspeed3B,
@@ -78,135 +113,268 @@ public class Batter extends Player{
         this.OPS = OPS;
     }
 
+    /**
+     * Get the numbers of PA against fastball.
+     * @return The numbers of PA against fastball
+     */
     public int getFastballPA() {
         return this.fastballPA;
     }
 
+    /**
+     * Get the numbers of hits against fastball.
+     * @return The numbers of hits against fastball
+     */
     public int getFastballH() {
         return this.fastballH;
     }
 
+    /**
+     * Get the numbers of singles against fastball.
+     * @return The numbers of singles against fastball
+     */
     public int getFastball1B() {
         return this.fastball1B;
     }
 
+    /**
+     * Get the numbers of doubles against fastball.
+     * @return The numbers of doubles against fastball
+     */
     public int getFastball2B() {
         return this.fastball2B;
     }
 
+    /**
+     * Get the numbers of triples against fastball.
+     * @return The numbers of triples against fastball
+     */
     public int getFastball3B() {
         return this.fastball3B;
     }
 
+    /**
+     * Get the numbers of homers against fastball.
+     * @return The numbers of homers against fastball.
+     */
     public int getFastballHR() {
         return this.fastballHR;
     }
 
+    /**
+     * Get the numbers of PA against breaking ball.
+     * @return The numbers of PA against breaking ball
+     */
     public int getBreakingPA() {
         return this.breakingPA;
     }
 
+    /**
+     * Get the numbers of hits against breaking ball.
+     * @return The numbers of hits against breaking ball
+     */
     public int getBreakingH() {
         return this.breakingH;
     }
 
+    /**
+     * Get the numbers of singles against breaking ball.
+     * @return The numbers of singles against breaking ball
+     */
     public int getBreaking1B() {
         return this.breaking1B;
     }
 
+    /**
+     * Get the numbers of doubles against breaking ball.
+     * @return The numbers of doubles against breaking ball
+     */
     public int getBreaking2B() {
         return this.breaking2B;
     }
 
+    /**
+     * Get the numbers of triples against breaking ball.
+     * @return The numbers of triples against breaking ball
+     */
     public int getBreaking3B() {
         return this.breaking3B;
     }
 
+    /**
+     * Get the numbers of homers against breaking ball.
+     * @return The numbers of homers against breaking ball
+     */
     public int getBreakingHR() {
         return this.breakingHR;
     }
 
+    /**
+     * Get the numbers of PA against offspeed ball.
+     * @return The numbers of PA against off-peed ball
+     */
     public int getOffspeedPA() {
         return this.offspeedPA;
     }
 
+    /**
+     * Get the numbers of hits against offspeed ball.
+     * @return The numbers of hits against offspeed ball.
+     */
     public int getOffspeedH() {
         return this.offspeedH;
     }
 
+    /**
+     * Get the numbers of singles against offspeed ball.
+     * @return The numbers of singles against offspeed ball
+     */
     public int getOffspeed1B() {
         return this.offspeed1B;
     }
 
+    /**
+     * Get the numbers of doubles against offspeed ball.
+     * @return The numbers of doubles against offspeed ball
+     */
     public int getOffspeed2B() {
         return this.offspeed2B;
     }
 
+    /**
+     * Get the numbers of triples against offspeed ball.
+     * @return The numbers of triples against offspeed ball
+     */
     public int getOffspeed3B() {
         return this.offspeed3B;
     }
 
+    /**
+     * Get the numbers of homers against offspeed ball.
+     * @return The numbers of homers against offspeed ball
+     */
     public int getOffspeedHR() {
         return this.offspeedHR;
     }
 
+    /**
+     * Get the total numbers of PA.
+     * @return The total numbers of PA
+     */
     public int getTotalPA() {
         return this.totalPA;
     }
 
+    /**
+     * Get the total numbers of hits.
+     * @return The total numbers of hits
+     */
     public int getTotalH() {
         return this.totalH;
     }
 
+    /**
+     * Get the total numbers of singles.
+     * @return The total numbers of singles
+     */
     public int getTotal1B() {
         return this.total1B;
     }
 
+    /**
+     * Get the total numbers of doubles.
+     * @return The total numbers of doubles
+     */
     public int getTotal2B() {
         return this.total2B;
     }
 
+    /**
+     * Get the total numbers of triples.
+     * @return The total numbers of triples
+     */
     public int getTotal3B() {
         return this.total3B;
     }
 
+    /**
+     * Get the total numbers of homers.
+     * @return The total numbers of homers
+     */
     public int getTotalHR() {
         return this.totalHR;
     }
 
+    /**
+     * Get the rate of swinging at pitches in the strike zone.
+     * @return The zone swing rate
+     */
     public double getZoneSwing() {
         return this.zoneSwing;
     }
 
+    /**
+     * Gets the rate of making contact on pitches in the strike zone.
+     * @return The zone contact rate
+     */
     public double getZoneContact() {
         return this.zoneContact;
     }
 
+    /**
+     * Get the rate of swinging at pitches outside the strike zone.
+     * @return The chase swing rate
+     */
     public double getChaseSwing() {
         return this.chaseSwing;
     }
 
+    /**
+     * Gets the rate of making contact on pitches outside the strike zone.
+     * @return The chase contact rate
+     */
     public double getChaseContact() {
         return this.chaseContact;
     }
 
+    /**
+     * Get the AVG of the batter.
+     * @return The AVG of the batter
+     */
     public double getAVG() {
         return this.AVG;
     }
 
+    /**
+     * Get the OBP of the batter.
+     * @return The OBP of the batter
+     */
     public double getOBP() {
         return this.OBP;
     }
 
+    /**
+     * Get the OPS of the batter.
+     * @return The OPS of the batter
+     */
     public double getOPS() {
         return this.OPS;
     }
 
+    /**
+     * Get the player type.
+     * @return The player type
+     */
     @Override
     public String getPosition() {
         return "Batter";
     }
 
+    /**
+     * Check if two batters are equal.
+     * @param obj Object to compare
+     * @return true if the objects are equal
+     */
     @Override
     public boolean equals(Object obj) {
         return EqualsBuilder.reflectionEquals(this, obj,
@@ -217,6 +385,10 @@ public class Batter extends Player{
                         "chaseSwing", "chaseContact", "AVG", "OBP", "OPS"));
     }
 
+    /**
+     * Get the hash code of the object
+     * @return The hash code of the object
+     */
     public int hashcode() {
         return HashCodeBuilder.reflectionHashCode(this,
                 List.of("fastballPA", "fastballH", "fastball1B", "fastball2B", "fastball13B", "fastballHR",
@@ -226,6 +398,10 @@ public class Batter extends Player{
                         "chaseSwing", "chaseContact", "AVG", "OBP", "OPS"));
     }
 
+    /**
+     * Info of the batter.
+     * @return A string contains info of the batter
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
