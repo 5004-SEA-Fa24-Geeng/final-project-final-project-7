0dfd83219ce6a6bc00d5d2081ee3e9ae63bc6e8c
Huang Shou Liu <liu.huangs@northeastern.edu>
2025-04-10 11:14:20 -0700

    feat: finish javadoc in PlayerTeam and ComTeam

--- src/main/java/model/team/ComTeam.java
+++ src/main/java/model/team/ComTeam.java
@@ -3,6 +3,10 @@ package model.team;
 import gameEnum.Teams;
 
 public class ComTeam extends Team{
+    /**
+     * Constructor for ComTeam
+     * @param teamName enum in Teams
+     */
     public ComTeam(Teams teamName) {
         super(teamName);
     }
